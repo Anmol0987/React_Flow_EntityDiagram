b6c5f099ce16984bbb8a77327c5b5c92a9b240d7
ayushchhanchar <chhancharayush007@gmail.com>
2024-10-08 13:35:36 +0530

    styled nodes

--- src/CustomNode.jsx
+++ src/CustomNode.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Handle, Position } from '@xyflow/react';
 import { AiOutlineDown } from 'react-icons/ai';
+import { FiTable } from "react-icons/fi";
 
 const CustomNode = ({ data,id }) => {
   const [collapsed, setCollapsed] = useState(false);
@@ -10,66 +11,72 @@ const CustomNode = ({ data,id }) => {
   const toggleSubCollapse = () => setSubCollapsed(!subCollapsed);
 
   return (
-    <div className="p-4 border border-blue-300 rounded bg-white shadow-md">
-      <div className="flex justify-between font-bold items-center">
-        <h3 className='text-2xl'>
+    <div className=" border  border-blue-300 rounded bg-[#0F172A] shadow-md">
+      <div className="flex justify-between gap-4 bg-[#8EB7FF] p-2   font-bold items-center">
+        <h3 className='flex items-center text-white gap-1 justify-center text-[3vh]'>
+          <FiTable/>
           {data.label}
           </h3>
-          <Handle type="source" id={`${id}`} position={Position.Top}  />
-          <Handle type="target" id={`${id}`} position={Position.Bottom}  />
-        <AiOutlineDown className="cursor-pointer" onClick={toggleCollapse} />
+          <Handle type="source" id={`${id}`} position={Position.Top} className='bg-[#8EB7FF] hover:scale-[2.8] transition-transform duration-200' />
+          <Handle type="target" id={`${id}`} position={Position.Bottom} className='bg-[#8EB7FF] hover:scale-[2.8] transition-transform duration-200' />
+        <AiOutlineDown className="cursor-pointer font-bold text-white" onClick={toggleCollapse} />
       </div>
+      
       {!collapsed && (
-        <div className="mt-2">
-          <div className="flex justify-between font-bold items-center">
+        <div className=" bg-[#0F172A] ">
+          <div className="flex justify-between gap-3 font-bold items-center py-2 pr-4 pl-1  text-white bg-[#05091A]">
             <h4>{data.subheading1}</h4>
             <AiOutlineDown className="cursor-pointer" onClick={toggleSubCollapse} />
           </div>
           {!subCollapsed && (
-            <ul>
+            <ul className='p-1'>
               {data.components1.map((component, index) => (
-                <li key={index}>
-                  <div className='bg-red-500'>
+                <li key={index} >
+                  <div className='text-white p-1 '>
                     {component.name}
                     <Handle
                       type="source"
                       position={Position.Right}
-                      id={`${component.id}`}
-                      style={{ background: 'red', top: 90 + index * 30 }}
+                      id={`${id}-${component.id}`}
+                      className='0F172A hover:scale-[2.8] transition-transform duration-200'
+                      style={{background: 'red',  top: 90 + index * 30 }}
                     />
                     <Handle
                       type="target"
                       position={Position.Left}
-                      id={`${component.id}`}
-                      style={{ background: 'red', top: 90 + index * 30 }}
+                      id={`${id}-${component.id}`}
+                      className='0F172A  hover:scale-[2.8] transition-transform  duration-200'
+                      style={{background: 'red',  top: 90 + index * 30 }}
                     />
                   </div>
                 </li>
               ))}
             </ul>
           )}
-          <div className="flex justify-between font-bold  items-center mt-2">
+          <div className="flex justify-between gap-3 font-bold items-center py-2 pr-4 pl-1  text-white bg-[#05091A]">
             <h4>
               {data.subheading2}
             </h4>
             <AiOutlineDown className="cursor-pointer" onClick={toggleSubCollapse} />
           </div>
           {!subCollapsed && (
-            <ul>
+            <ul className='p-1'>
               {data.components2.map((component, index) => (
-                <li className='bg-orange-400' key={index}>
+                <li className='text-white p-1 ' key={index}>
                   {component.name}
                   <Handle
                     type="source"
                     position={Position.Right}
-                    id={`${component.id}`}
+                    id={`${id}-${component.id}`}
                     style={{ background: 'orange', top: 145 + index * 30 }}
+                     className='0F172A  hover:scale-[2.8] transition-transform  duration-200'
                   />
                   <Handle
                     type="target"
                     position={Position.Left}
-                    id={`${component.id}`}
+                    id={`${id}-${component.id}`}
                     style={{ background: 'orange', top: 145 + index * 30 }}
+                     className='0F172A  hover:scale-[2.8] transition-transform  duration-200'
                   />
                 </li>
               ))}
--- src/ERDiagram.jsx
+++ src/ERDiagram.jsx
@@ -18,13 +18,16 @@ const initialNodes = [
     id: '1',
     type: 'collapsibleNode',
     data: {
+      
       label: 'Node 1',
       subheading1: 'Subheading 1.1',
-      components1: [{ name: 'Component A', id: "c1" }, { name: 'Component B', id: "c2" }],
+      components1: [
+        { id: 'C1', name: 'Component A' },
+        { id: 'C2', name: 'Component B' }
+      ],
       subheading2: 'Subheading 1.2',
-      components2: [{name:'Component C',id:"c3"}],
+      components2: [{ id: 'C3', name: 'Component C' }],
     },
-    background: 'lightblue',
     position: { x: 100, y: 100 },
   },
   {
@@ -33,11 +36,14 @@ const initialNodes = [
     data: {
       label: 'Node 2',
       subheading1: 'Subheading 2.1',
-      components1: [{name:'Component D',id:"c4"}],
+      components1: [{ id: 'C4', name: 'Component D' }],
       subheading2: 'Subheading 2.2',
-      components2: [{name:'Component E',id:"c5"}, {name:'Component F',id:"c6"}],
+      components2: [
+        { id: 'C5', name: 'Component E' },
+        { id: 'C6', name: 'Component F' },
+      ],
     },
-    position: { x: 100, y: 400 },
+    position: { x: 400, y: 100 },
   },
   // {
   //   id: '3',
@@ -66,8 +72,10 @@ const initialNodes = [
 ];
 const initialEdges = [
   { id: 'e1', source: '1', target: '2' ,markerEnd: {type: 'arrowclosed'}, },
-  { id: 'e2', source: `${data.components1}.c1`, target: `${data.components1}.c4` ,markerEnd: {type: 'arrowclosed'}, },
-];
+  // { id: 'e2', source: '1-c1', target: '2-c4', markerEnd: { type: 'arrowclosed' } },
+  { id: 'e1-2-B-E', source: '1-componentB', target: '2-componentE', animated: true },
+  { id: 'e1-2-C-F', source: '1-componentC', target: '2-componentF', animated: true },
+]; 
 
 
 const ERDiagram = () => {
@@ -81,7 +89,7 @@ const ERDiagram = () => {
   );
 
   return (
-    <div style={{background: "#111827", width: '100vw', height: '100vh' }}>
+    <div style={{background: "black", width: '100vw', height: '100vh' }}>
       <ReactFlow
         nodes={nodes}
         edges={edges}
